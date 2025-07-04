77bd1c8e4765b2bb6db819a91d45b4ed4badde4e
wlzh <mswnlz@gmail.com>
2025-07-04 12:15:13 +0800

    feat: 添加 VitePress 配置文件以支持网站验证

---
+++ .vitepress/config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  head: [
+    ['meta', { name: 'google-site-verification', content: 'lI-wB0SQ6fXo-tUmUtTvz_9Qa65EMnPl_9PUuxhCJoI' }]
+  ]
+})
