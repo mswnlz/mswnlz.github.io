d9c6385b0c8acab070b2f0fb94f5028ee8a8b066
wlzh <mswnlz@gmail.com>
2025-07-04 11:56:50 +0800

    feat: 添加资源分享QQ群链接到导航菜单

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
   ],
   themeConfig: {
     nav: [
+      { text: '加入资源分享QQ群：1041415822' },
       { text: '首页', link: '/' },
       { text: '所有资源', link: '/AIknowledge/' } // 指向第一个资源分类，用户可以通过侧边栏切换
     ],
