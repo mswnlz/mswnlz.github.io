0df4269e62fd8b3d832f5241da45a0316744f0f8
wlzh <mswnlz@gmail.com>
2025-07-06 21:20:27 +0800

    Deploy website with updated content

--- .vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import CommitHistory from './CommitHistory.vue'
-
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('CommitHistory', CommitHistory)
-  }
-}
